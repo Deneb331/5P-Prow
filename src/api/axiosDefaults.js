89348b5d3e323b686417db7bfa9f2d6d650f3746
Deneb331 <juanjo1.seriousmail@gmail.com>
2023-06-16 12:34:47 +0000

    Create navbar and connect with api

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://prowapi-5p.herokuapp.com'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
+
