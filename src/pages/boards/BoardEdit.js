71cf729f57067051b5473e69b375d573d337e9c6
Deneb331 <juanjo1.seriousmail@gmail.com>
2023-06-20 11:50:48 +0000

    Add bords edit page with delete function

--- src/pages/boards/BoardEdit.js
+++ src/pages/boards/BoardEdit.js
@@ -15,27 +15,50 @@ import btnStyles from "../../styles/Button.module.css";
 import { useHistory, useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
-function PostEditForm() {
+function BoardEdit() {
   const [errors, setErrors] = useState({});
+  const [workspaces, setWorkspaces] = useState([]);
 
-  const [postData, setPostData] = useState({
+  const [boardData, setBoardData] = useState({
     title: "",
-    content: "",
+    workspace: "",
     image: "",
   });
-  const { title, content, image } = postData;
+  const { title, workspace, image } = boardData;
 
   const imageInput = useRef(null);
   const history = useHistory();
   const { id } = useParams();
 
   useEffect(() => {
+    fetchWorkspaces();
+  }, []);
+
+  const fetchWorkspaces = async () => {
+    try {
+      const response = await axiosReq.get("/workspaces");
+      const responseData = response.data;
+
+      if (Array.isArray(responseData.results)) {
+        setWorkspaces(responseData.results);
+      } else {
+        console.error("Invalid workspace data:", responseData);
+        setWorkspaces([]);
+      }
+    } catch (error) {
+      console.error("Error fetching workspaces:", error);
+    }
+  };
+
+  useEffect(() => {
     const handleMount = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/${id}/`);
-        const { title, content, image, is_owner } = data;
+        const { data } = await axiosReq.get(`/boards/${id}`);
+        const { title, workspace, image, is_owner } = data;
 
-        is_owner ? setPostData({ title, content, image }) : history.push("/");
+        is_owner
+          ? setBoardData({ title, workspace, image })
+          : history.push("/");
       } catch (err) {
         console.log(err);
       }
@@ -45,8 +68,8 @@ function PostEditForm() {
   }, [history, id]);
 
   const handleChange = (event) => {
-    setPostData({
-      ...postData,
+    setBoardData({
+      ...boardData,
       [event.target.name]: event.target.value,
     });
   };
@@ -54,8 +77,8 @@ function PostEditForm() {
   const handleChangeImage = (event) => {
     if (event.target.files.length) {
       URL.revokeObjectURL(image);
-      setPostData({
-        ...postData,
+      setBoardData({
+        ...boardData,
         image: URL.createObjectURL(event.target.files[0]),
       });
     }
@@ -66,20 +89,29 @@ function PostEditForm() {
     const formData = new FormData();
 
     formData.append("title", title);
-    formData.append("content", content);
+    formData.append("workspace", workspace);
 
     if (imageInput?.current?.files[0]) {
       formData.append("image", imageInput.current.files[0]);
     }
 
     try {
-      await axiosReq.put(`/posts/${id}/`, formData);
-      history.push(`/posts/${id}`);
+      await axiosReq.put(`/boards/${id}`, formData);
+      history.push(`/boards/${id}`);
     } catch (err) {
       console.log(err);
       if (err.response?.status !== 401) {
         setErrors(err.response?.data);
       }
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axiosReq.delete(`/boards/${id}`);
+      history.push("/");
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -101,14 +133,20 @@ function PostEditForm() {
       ))}
 
       <Form.Group>
-        <Form.Label>Content</Form.Label>
+        <Form.Label>Workspace</Form.Label>
         <Form.Control
-          as="textarea"
-          rows={6}
-          name="content"
-          value={content}
+          as="select"
+          name="workspace"
+          value={workspace}
           onChange={handleChange}
-        />
+        >
+          <option value="">Select a workspace</option>
+          {workspaces.map((workspace) => (
+            <option key={workspace.id} value={workspace.id}>
+              {workspace.title}
+            </option>
+          ))}
+        </Form.Control>
       </Form.Group>
       {errors?.content?.map((message, idx) => (
         <Alert variant="warning" key={idx}>
@@ -117,13 +155,16 @@ function PostEditForm() {
       ))}
 
       <Button
-        className={`${btnStyles.Button} ${btnStyles.Blue}`}
+        className={`${btnStyles.Button} ${btnStyles.Grey}`}
         onClick={() => history.goBack()}
       >
         cancel
       </Button>
-      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+      <Button className={`${btnStyles.Button} ${btnStyles.Grey}`} type="submit">
         save
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Red}`} onClick={handleDelete}>
+        delete
       </Button>
     </div>
   );
@@ -131,7 +172,7 @@ function PostEditForm() {
   return (
     <Form onSubmit={handleSubmit}>
       <Row>
-        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+        <Col className="py-2 p-0 p-md-2" md={12}>
           <Container
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
@@ -164,7 +205,7 @@ function PostEditForm() {
             <div className="d-md-none">{textFields}</div>
           </Container>
         </Col>
-        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+        <Col md={12} className="d-none d-md-block p-0 p-md-2">
           <Container className={appStyles.Content}>{textFields}</Container>
         </Col>
       </Row>
@@ -172,4 +213,4 @@ function PostEditForm() {
   );
 }
 
-export default PostEditForm;
+export default BoardEdit;
